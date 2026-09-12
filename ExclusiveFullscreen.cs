fe8fd3f3df285ede6df4fb4c5c37cafc08b6eec2
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-09-11 22:20:53 -0400

    Initialize

---
+++ ExclusiveFullscreen.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace ExclusiveFullscreen
+{
+    public class ExclusiveFullscreen : ModBehaviour
+    {
+        public static ExclusiveFullscreen Instance;
+
+        public void Awake()
+        {
+            Instance = this;
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        public void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(ExclusiveFullscreen)} is loaded!", MessageType.Success);
+
+            new Harmony("MegaPiggy.ExclusiveFullscreen").PatchAll(Assembly.GetExecutingAssembly());
+
+            // Example of accessing game code.
+            OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+        }
+
+        public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+        {
+            if (newScene != OWScene.SolarSystem) return;
+            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Outer Wilds Exclusive Fullscreen
+A mod that forces Outer Wilds to use exclusive fullscreen instead of borderless fullscreen.
