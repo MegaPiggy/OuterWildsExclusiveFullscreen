c826aad508e11fbd9fca0094f4ad900f422a0401
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-09-11 22:21:53 -0400

    patch

--- ExclusiveFullscreen.cs
+++ ExclusiveFullscreen.cs
@@ -40,3 +40,50 @@ namespace ExclusiveFullscreen
     }
 
 }
+
+
+[HarmonyPatch(typeof(GraphicSettings), nameof(GraphicSettings.ApplyAllGraphicSettings))]
+public static class GraphicSettings_ApplyAllGraphicSettings_Patch
+{
+    private static readonly MethodInfo OriginalSetResolution = typeof(Screen).GetMethod(
+        nameof(Screen.SetResolution),
+        new[]
+        {
+            typeof(int),
+            typeof(int),
+            typeof(bool)
+        });
+
+    private static readonly MethodInfo ReplacementSetResolution = typeof(GraphicSettings_ApplyAllGraphicSettings_Patch).GetMethod(
+        nameof(SetResolution),
+        BindingFlags.Static | BindingFlags.NonPublic);
+
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            if (instruction.Calls(OriginalSetResolution))
+            {
+                yield return new CodeInstruction(OpCodes.Call, ReplacementSetResolution)
+                    .WithLabels(instruction.labels)
+                    .WithBlocks(instruction.blocks);
+            }
+            else
+            {
+                yield return instruction;
+            }
+        }
+    }
+
+    private static void SetResolution(int width, int height, bool fullScreen)
+    {
+        Screen.SetResolution(
+            width,
+            height,
+            fullScreen
+                ? FullScreenMode.ExclusiveFullScreen
+                : FullScreenMode.Windowed
+        );
+    }
+}
